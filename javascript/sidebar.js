20f37d0e8979d16f5a21eef8ed1c0926ba322850
smeessathit <smeessathit@gmail.com>
2017-08-28 15:01:50 +0700

    Web built with W3 CSS Signed-off-by: smeessathit <smeessathit@gmail.com>

---
+++ javascript/sidebar.js
@@ -0,0 +1,11 @@
+// Side navigation
+			function w3_open() {
+			    var x = document.getElementById("mySidebar");
+			    x.style.width = "30%";
+			    x.style.fontSize = "30px";
+			    x.style.paddingTop = "10%";
+			    x.style.display = "block";
+			}
+			function w3_close() {
+			    document.getElementById("mySidebar").style.display = "none";
+			}
